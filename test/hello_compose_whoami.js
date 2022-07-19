4a20c5686da52a19a0b4764e162ee3a3665e5cdc
louiscklaw <louiscklaw@users.noreply.github.com>
2022-07-19 13:55:36 +0800

    update,

--- test/hello_compose_whoami.js
+++ test/hello_compose_whoami.js
@@ -16,24 +16,24 @@ describe('hello_compose_whoami', function () {
         done();
       })();
     });
+
+    it("should pull all needed images silently", function (done) {
+      this.timeout(600000);
+      (async () => {
+        await compose_whoami.pull();
+        done();
+      })();
+    });
+
+    it("should pull all needed images returning streams", function (done) {
+      this.timeout(600000);
+      (async () => {
+        var streams = await compose_whoami.pull(null, { 'streams': true });
+        expect(streams).to.be.ok;
+        done();
+      })();
+    });
   })
-
-  it("should pull all needed images silently", function (done) {
-    this.timeout(600000);
-    (async () => {
-      await compose_whoami.pull();
-      done();
-    })();
-  });
-
-  it("should pull all needed images returning streams", function (done) {
-    this.timeout(600000);
-    (async () => {
-      var streams = await compose_whoami.pull(null, { 'streams': true });
-      expect(streams).to.be.ok;
-      done();
-    })();
-  });
 
   describe('#up', function () {
     it("should do compose up", function (done) {
@@ -65,12 +65,12 @@ describe('hello_compose_whoami', function () {
       this.timeout(60000);
       (async () => {
         await compose_whoami.down({volumes: true});
-        let listContainers = await docker.listContainers({ 'all': true, 'filters': {"label":[`com.docker.compose.project=${compose.projectName}`]}});
+        let listContainers = await docker.listContainers({ 'all': true, 'filters': {"label":[`com.docker.compose.project=${compose_whoami.projectName}`]}});
         expect(listContainers).to.be.empty
-        let listVolumes  = await docker.listVolumes({ 'filters': {"label":[`com.docker.compose.project=${compose.projectName}`]}})
+        let listVolumes  = await docker.listVolumes({ 'filters': {"label":[`com.docker.compose.project=${compose_whoami.projectName}`]}})
         expect(listVolumes.Volumes).to.be.empty
         expect(listVolumes.Warnings).to.be.null
-        let listNetworks = await docker.listNetworks({ 'filters': {"label":[`com.docker.compose.project=${compose.projectName}`]}})
+        let listNetworks = await docker.listNetworks({ 'filters': {"label":[`com.docker.compose.project=${compose_whoami.projectName}`]}})
         expect(listNetworks).to.be.empty
         done();
       })();
